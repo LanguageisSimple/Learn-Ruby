2ea76b67eaf2c960d6e0d6ba3a297c6288c0d7b5
BINDUPAUTRA JYOTIBRAT <jyotibratb@gmail.com>
2024-06-14 22:58:23 +0530

    Uploading "Types_of_Printing_in_Terminal.rb" file
    Adding the file which contains resources abouts how to print in ruby programming language.

---
+++ src/Print Statements/Types_of_Printing_in_Terminal.rb
@@ -0,0 +1,22 @@
+#For Printing and not a new line i.e. after print what is inside the print statement will not move to the new line we have to use the 'print' keyword.
+
+print("Hello World")
+
+=begin
+The 'print' statement is quite similar to python.
+But here is the catch that another way to use the 'print' keyword is --> ::syntax:: [print "{Write what you want to write}"]. 
+Also remember that the 'print' statement will not work when use curly braces '{}'.
+=end
+
+print "Hello World"
+
+=begin
+Now We move to another way to print things in Ruby.
+We can use another keyword to print things i.e., 'puts'.
+It works just like 'print', but it adds a newline character at the end of what you print.
+It works for normal braces and no braces, but not curly braces.
+=end
+
+puts "Hello World"
+
+puts("Hello World")
